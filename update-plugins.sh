0e2cb222e228ee5334292982341291745dc2b292
Dusko Simidzija <dusko.simidzija@gmail.com>
2018-08-18 21:14:39 +0200

    fix update-plugins script

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
-git submodule foreach git submodule --init --recursive
+git submodule foreach git submodule init
 git submodule foreach git submodule update --recursive
