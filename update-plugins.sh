1a8573be431ad16faa25951124736e8b9136c71f
Dusko Simidzija <dusko.simidzija@gmail.com>
2019-10-12 21:57:43 +0200

    updated plugin update scripts

--- update-plugins.sh
+++ update-plugins.sh
@@ -5,4 +5,5 @@ git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
 git submodule foreach git submodule init
+git submodule foreach git submodule sync --recursive
 git submodule foreach git submodule update --recursive
